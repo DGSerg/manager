119fe05c9c712c5e6249f0af33b8446b14bcf89d
DGSerg <Karpenko.serg@mail.ru>
2017-11-15 22:53:47 +0200

    added

---
+++ main_project/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	//private static final Logger LOG = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) throws InterruptedException {
+		StartPage st = new StartPage();
+
+	//	LOG.info("Application started");
+		//System.out.println("I'm the main project");
+
+//		Simple simple = new Simple();
+//		simple.setValue(10);
+//		System.out.println("Value from Simple: " + simple.getValue());
+		//LOG.info("Application finished");
+	}
+}
---
+++ main_project/src/main/java/model/ClientRecord.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by RaidMax on 23.09.2017.
+ */
+public class ClientRecord {
+
+}
